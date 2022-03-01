2890efddb4d6b1ded69aaf979582450886d4ed92
acohen061604 <63744416+acohen061604@users.noreply.github.com>
2022-03-01 10:11:42 -0500

    new

---
+++ server.js
@@ -0,0 +1,39 @@
+//..............Include Express..................................//
+const express = require('express');
+const fs = require('fs');
+const ejs = require('ejs');
+
+//..............Create an Express server object..................//
+const app = express();
+
+//..............Apply Express middleware to the server object....//
+app.use(express.json()); //Used to parse JSON bodies (needed for POST requests)
+app.use(express.urlencoded());
+app.use(express.static('public')); //specify location of static assests
+app.set('views', __dirname + '/views'); //specify location of templates
+app.set('view engine', 'ejs'); //specify templating library
+
+//.............Define server routes..............................//
+//Express checks routes in the order in which they are defined
+
+app.get('/', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html');
+  response.render("index");
+});
+// Because routes/middleware are applied in order,
+// this will act as a default error route in case of
+// a request fot an invalid route
+app.use("", function(request, response){
+  response.status(404);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("error", {
+    "errorCode":"404"
+  });
+});
+
+//..............Start the server...............................//
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server started at http://localhost:'+port+'.')
+});
